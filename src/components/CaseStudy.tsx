80c434fa3b81bd8a792aa3f4cba90ac2dc2fe546
deadpine <hey@deadpine.xyz>
2025-08-08 20:06:50 -0300

    Update CaseStudy.tsx

--- src/components/CaseStudy.tsx
+++ src/components/CaseStudy.tsx
@@ -9,7 +9,7 @@ interface CaseProps {
 
 export default function Case({ builtfor, logo, subtitle, description }: CaseProps) {
   return (
-    <div className="w-full flex flex-col gap-8 p-2 md:p-8 overflow-hidden">  
+    <div className="w-full flex flex-col gap-8 px-2 py-10 md:p-8 overflow-hidden">  
       <div className="w-full flex flex-col items-center justify-center relative">
         <Image
           src={logo}
