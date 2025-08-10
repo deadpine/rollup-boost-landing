052faf8bb231d24ee4ea9f8d95c92de3b3352b9d
deadpine <hey@deadpine.xyz>
2025-08-09 23:11:45 -0300

    update social images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,32 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Rollup Boost",
   description: "Upgrade your rollups across performance, programmability, and decentralization",
+
+  openGraph: {
+    title: "Rollup Boost",
+    description: "Upgrade your rollups across performance, programmability, and decentralization",
+    // Update URL
+    url: "https://rollup-boost-landing.vercel.app",
+    siteName: "Rollup Boost",
+    images: [
+      {
+        url: "https://rollup-boost-landing.vercel.app/images/rollupboost_cover.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Rollup Boost cover",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Rollup Boost",
+    description: "Upgrade your rollups across performance, programmability, and decentralization",
+    // Update URL
+    images: ["https://rollup-boost-landing.vercel.app/images/rollupboost_cover.jpg"],
+  },
 };
 
 export default function RootLayout({
