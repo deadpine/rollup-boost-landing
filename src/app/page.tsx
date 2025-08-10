532c1236e9e54bf123b5560c615e99c4390c5bf1
deadpine <hey@deadpine.xyz>
2025-08-09 22:42:34 -0300

    mobile updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     // <div className=" grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-    <main className="font-sans flex flex-col items-center sm:items-start space-y-4 md:space-y-10 px-6 md:px-8">
+    <main className="font-sans flex flex-col items-center sm:items-start space-y-4 md:space-y-14 px-6 md:px-8">
       
       <nav className="w-full flex justify-between pt-6 pb-2 md:p-8">
         <Image 
@@ -37,7 +37,14 @@ export default function Home() {
       </nav>
 
       <section id="home" className="w-full grid grid-cols-1 md:grid-cols-2 items-center">
-        <div className="h-full flex flex-col justify-between md:p-8">
+        <Image 
+          src="/images/hero.svg" 
+          alt="" 
+          width={500} 
+          height={500} 
+          className="w-full h-auto max-w-fill max-h-160 md:order-2"
+        />
+        <div className="h-full flex flex-col justify-between md:p-8 md:order-1">
           <h1 className="max-w-xl text-black font-semibold text-4xl md:text-5xl pt-4">
             Upgrade your rollups across performance, programmability, and decentralization.
           </h1>
@@ -77,16 +84,9 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <Image 
-          src="/images/hero.svg" 
-          alt="" 
-          width={500} 
-          height={500} 
-          className="w-full h-auto max-w-fill max-h-160  "
-        />
       </section>
 
-      <section id="rollup-boost" className="w-full gap-5 items-center py-6 md:py-12">
+      <section id="rollup-boost" className="w-full gap-5 items-center py-4 md:py-12">
         <div className="max-w-3xl mx-auto text-center py-12">
           <h2 className="text-black font-semibold text-3xl md:text-4xl pb-4">
             Verifiable priority within each Flashblock, offering stronger guarantees and enabling apps to internalize MEV.
@@ -119,7 +119,7 @@ export default function Home() {
         </div>
       </section>
 
-      <section id="use-cases" className="w-full gap-5 items-center py-6 md:py-12">
+      <section id="use-cases" className="w-full gap-5 items-center py-4">
         <div className="max-w-3xl mx-auto text-center py-12">
           <h2 className="text-black font-semibold text-3xl md:text-4xl pb-4">
             Use cases: guarantees to users and allowing applications to internalize their MEV
@@ -156,8 +156,15 @@ export default function Home() {
         </div>
       </section>
 
-      <section id="flashblocks" className="w-full grid grid-cols-1 md:grid-cols-2 gap-5 items-center">
-        <div className="px-8">
+      <section id="flashblocks" className="w-full grid grid-cols-1 md:grid-cols-2 gap-5 items-center py-16 md:py-4">
+        <Image 
+          src="/images/flashblocks-section.png" 
+          alt="" 
+          width={500} 
+          height={500} 
+          className="w-full max-w-fill h-90 md:h-auto md:max-h-140 object-cover md:object-contain md:order-2"
+        />
+        <div className="md:px-8 md:order-1">
           <Chip label="Flashblocks" />
           <div className="pt-8 pb-5">
             <h2 className="max-w-xl text-black font-semibold text-4xl py-4">
@@ -187,26 +194,18 @@ export default function Home() {
             title="Composability"
             description="Seamlessly integrate layers for unified liquidity"
           />
-          
         </div>
-        <Image 
-          src="/images/flashblocks-section.png" 
-          alt="" 
-          width={500} 
-          height={500} 
-          className="h-160 w-auto max-w-fill mx-auto"
-        />
       </section>
 
-      <section id="tees" className="w-full grid grid-cols-1 md:grid-cols-2 gap-5 items-center">
+      <section id="tees" className="w-full grid grid-cols-1 md:grid-cols-2 gap-5 items-center py-6">
         <Image 
           src="/images/tees-section.png" 
           alt="" 
           width={500} 
           height={500} 
-          className="h-160 w-auto max-w-fill"
+          className="w-full max-w-fill h-90 md:h-auto md:max-h-140 object-cover md:object-contain"
         />
-        <div className="px-8">
+        <div className="md:px-8">
           <Chip label="Tees" />
           <h2 className="text-black font-semibold text-4xl pt-12 pb-8">
             Decentralizing Ethereum’s Last Mile
@@ -223,20 +222,20 @@ export default function Home() {
         </div>
       </section>
 
-      <section id="contact" className="w-full flex flex-col gap-7 p-16 items-center rounded-[96px] bg-[#F1F2F3]">
+      <section id="contact" className="w-full flex flex-col gap-7 my-8 px-4 py-10 md:py-16 items-center rounded-3xl md:rounded-[96px] bg-[#F1F2F3]">
         <h2 className="text-black font-semibold text-4xl">
           Explore Our Tech
         </h2>
-        <div className="flex gap-4">
+        <div className="flex flex-col items-center sm:flex-row gap-4">
           <a 
             href="https://github.com/flashbots/rollup-boost" target="_blank" 
-            className="px-8 py-4 font-mono font-meium uppercase rounded-full border border-[#1A1D16] bg-[#1A1D16] text-white hover:bg-[#394046]"
+            className="w-fit px-8 py-4 font-mono font-meium uppercase rounded-full border border-[#1A1D16] bg-[#1A1D16] text-white hover:bg-[#394046]"
           >
             Try Testnet →
           </a>
           <a 
             href="https://github.com/flashbots/rollup-boost" target="_blank" 
-            className="px-8 py-4 font-mono font-meium uppercase rounded-full border border-[#1A1D16] text-black hover:bg-[#FFFFFF80] hover:text-[#1A1D16] hover:border-[#505A62]"
+            className="w-fit px-8 py-4 font-mono font-meium uppercase rounded-full border border-[#1A1D16] text-black hover:bg-[#FFFFFF80] hover:text-[#1A1D16] hover:border-[#505A62]"
           >
             Explore Github →
           </a>
@@ -247,7 +246,7 @@ export default function Home() {
         </p>
       </section>
 
-      <footer className="w-full flex items-center justify-between p-8">
+      <footer className="w-full flex flex-col sm:flex-row gap-4 items-center justify-between py-8 md:p-8">
         <Image 
           src="/images/flashbots_logo.svg" 
           alt="Flashbots logo" 
@@ -255,8 +254,8 @@ export default function Home() {
           height={25} 
           className="w-auto h-[25px]"
         />
-        <p className="font-mono uppercase text-xs">
-          Copyright © 2025 flashbots - All Rights Reserved
+        <p className="font-mono uppercase text-xs text-center">
+          Copyright © 2025 flashbots - <br className="sm:hidden" /> All Rights Reserved
         </p>
       </footer>
 
